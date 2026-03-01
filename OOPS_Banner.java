b3936f40b7842a8f4c6cbb0c5af0e35ae04591f3
sudharsanaa12 <sa0683@srmist.edu.in>
2026-03-01 19:38:19 +0530

    Add OOPS_Banner class to print a banner
    This class prints a decorative banner to the console.

---
+++ OOPS_Banner.java
@@ -0,0 +1,11 @@
+public class OOPS_Banner {
+    public static void main(String[] args) {
+        System.out.println("    **    " + " " + "    **    " + " " + "********" + " " + "    ****");
+        System.out.println("  **  **  " + " " + "  **  **  " + " " + "**    **" + " " + "  **     ");
+        System.out.println("**      **" + " " + "**      **" + " " + "**    **" + " " + "**      ");
+        System.out.println("**      **" + " " + "**      **" + " " + "********" + " " + " *****  ");
+        System.out.println("**      **" + " " + "**      **" + " " + "**      " + " " + "      **");
+        System.out.println("    **    " + " " + "    **    " + " " + "**      " + " " + "**  **  ");
+        System.out.println("  **   ** " + " " + "  **   ** " + " " + "**      " + " " + "  **    ");
+    }
+}
