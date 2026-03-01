44122026e59f2be0017802b2ddee90439d8bf225
sudharsanaa12 <sa0683@srmist.edu.in>
2026-03-01 19:40:08 +0530

    Add OOPS_Banner2 class with ASCII art output

---
+++ OOPS_Banner2.java
@@ -0,0 +1,12 @@
+public class OOPS_Banner2 {
+
+    public static void main(String[] args) {
+        System.out.println(String.join(" ","    **    ","    **    ","********","    ****"));
+        System.out.println(String.join(" ","  **  **  ","  **  **  ","**    **","  **    "));
+        System.out.println(String.join(" ","**      **","**      **","**    **","**      "));
+        System.out.println(String.join(" ","**      **","**      **","********"," *****  "));
+        System.out.println(String.join(" ","**      **","**      **","**      ","      **"));
+        System.out.println(String.join(" ","  **   ** ","  **   ** ","**      ","**   ** "));
+        System.out.println(String.join(" ","    **    ","    **    ","**      ","  ****"));
+    }
+}
