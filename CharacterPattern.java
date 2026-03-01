351b7a646c62b8067bed8985f37ad213cd2b583d
sudharsanaa12 <sa0683@srmist.edu.in>
2026-03-01 19:44:10 +0530

    Create CharacterPattern class with character and pattern

---
+++ CharacterPattern.java
@@ -0,0 +1,17 @@
+public class CharacterPattern {
+    private char character;
+    private String[] pattern;
+
+    public CharacterPattern(char character, String[] pattern) {
+        this.character = character;
+        this.pattern = pattern;
+    }
+
+    public char getCharacter() {
+        return character;
+    }
+
+    public String[] getPattern() {
+        return pattern;
+    }
+}
